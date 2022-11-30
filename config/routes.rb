212e0e9313045fec17673bc418a3a2b28178f112
Mutuma Gitonga <mgitonga150@gmail.com>
2022-11-30 09:41:49 +0300

    add restaurant_pizzas create resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   resources :restaurants, only: [:index, :show, :destroy]
   resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create]
 
 end
