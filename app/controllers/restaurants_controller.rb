68d06e4bc6584caf3adcc1a6713979464bcc72ee
Mutuma Gitonga <mgitonga150@gmail.com>
2022-11-30 08:13:11 +0300

    add restaurants controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+  def index 
+    @restaurants = Restaurant.all 
+    render json: @restaurants, status: :ok 
+    
+    # json_response(@restaurants, status = 200)
+  end
+
+  def show 
+    @restaurant = Restaurant.find(params[:id])
+    render json: @restaurant, status: :ok 
+  end
+
+  def destroy 
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    head :no_content
+  end
+
+
+  private
+  def render_record_not_found 
+    render json: {error: "Restaurant not found"}, status: :not_found
+  end
+end
